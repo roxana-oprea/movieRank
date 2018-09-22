d1288c87de253c1016a824142d936789ac4cbc35
roxana-oprea <oprearoxanaadolfina@gmail.com>
2018-09-22 20:13:52 +0300

    populate table with data from db

--- js/index.js
+++ js/index.js
@@ -1,6 +1,33 @@
 (function () {
   'use strict';
 
+  // populate table
+  function populateTable() {
+    const templateElement = document.querySelector('#movie-row');
+    const tbodyElement = document.querySelector('tbody');
+
+
+    fetch('http://localhost:3000/movies').then(response => response.json()).then(movies => {
+
+      for (let movie of movies) {
+        let clone = document.importNode(templateElement.content, true);
+        let tdElements = clone.querySelectorAll('td');
+
+        tdElements[0].textContent = movie.name;
+        tdElements[1].textContent = movie.imdbRating;
+        tdElements[2].textContent = movie.genre;
+        tdElements[3].textContent = movie.director;
+        tdElements[4].textContent = movie.year;
+        tdElements[5].textContent = movie.seen;
+
+        tbodyElement.appendChild(clone);
+      }
+    });
+
+  }
+
+  populateTable();
+
   // get references
   const searchElement = document.querySelector('#search');
   const tableElement = document.querySelector('table');
@@ -18,5 +45,6 @@
       row.hidden = !isMatch;
     }
   }
+
 
 })();
